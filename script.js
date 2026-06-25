e8bcc41a8c1287e3fb3ffe165547a8a0944046f7
codewithchristianpy-ship-it <codewithchristian.py@gmail.com>
2026-06-25 07:30:50 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let quotes = [
+  "The best way to get started is to quit talking and begin doing.",
+  "Don’t let yesterday take up too much of today.",
+  "It’s not whether you get knocked down, it’s whether you get up.",
+  "If you are working on something exciting, it will keep you motivated."
+];
+
+function newQuote() {
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quote").innerText = quotes[randomIndex];
+}
